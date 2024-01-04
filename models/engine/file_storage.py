d563e7587a7f4af0a8b722f367ffc575f48e7331
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-04 16:40:59 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,7 +38,8 @@ class FileStorage:
         """returns the an obj from __objects"""
         if cls is not None and id is not None:
             for key, value in self.__objects.items():
-                if (cls == value.__class__ or cls == value.__class__.__name__) and
+                if (cls == value.__class__ or \
+                    cls == value.__class__.__name__) and \
                    (value.id == id):
                     return (value)
         return (None)
