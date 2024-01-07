dbc26d8d414dfd89ea8199d87e5947a467173dcd
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-07 14:59:28 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,8 @@ from flask import Blueprint, jsonify, abort, request
 
 app_views = Blueprint('ap_views', __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places import *
+from api.v1.views.states import *
