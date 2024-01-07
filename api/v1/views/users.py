e950a2315856abfa561ec1fc2bc1589c7bc66105
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-07 14:28:51 +0300

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,47 +4,49 @@ Index for V1
 """
 from api.v1.views import app_views, jsonify, abort, request
 from models import storage
-from models.state import State
+from models.user import User
 
 
-@app_views.route('/states', methods = ['POST', 'GET'])
-def all_states():
+@app_views.route('/users', methods=['POST', 'GET'], strict_slashes=False)
+def all_users():
   """
-  Returns a list of all states
+  Returns a list of all users
   """
   if request.method == 'GET':
-    states = [i.to_dict() for i in storage.all(State).values()]
-    return (jsonify(states))
+    users = [i.to_dict() for i in storage.all(User).values()]
+    return (jsonify(users))
   if request.method == 'POST':
     if not request.is_json:
       return abort(400, 'Not a JSON')
     sud = request.get_json()
-    newstate_obj = State(**sud)
-    newstate_obj.save()
-    return ((jsonify(newstate_obj.to_dict()), 200))
+    if 'email' not in sud.keys():
+      abort(400, 'Missing email')
+    if 'password' not in sud.keys():
+      abort(400, 'Missing password')
+    newuser_obj = User(**sud)
+    newuser_obj.save()
+    return ((jsonify(newuser_obj.to_dict()), 201))
 
-@app_views.route('/states/<state_id>', methods = ['POST', 'GET', 'DELETE'])
-def rud_state(state_id):
+@app_views.route('/users/<user_id>', methods=['PUT', 'GET', 'DELETE'])
+def rud_user(user_id):
   """
-  Get/Modify/Delete state with id <state_id>
+  Get/Modify/Delete user with id <user_id>
   if present else returns raises error 404
   """
-  state_obj = storage.get(State,state_id)
-  if state_obj is None:
+  user_obj = storage.get(User,user_id)
+  if user_obj is None:
     abort(404)
   if request.method == 'GET':
-    return (jsonify(state_obj.to_dict()))
-  if request.method == 'POST':
+    return (jsonify(user_obj.to_dict()))
+  if request.method == 'PUT':
     if not request.is_json:
       return abort(400, 'Not a JSON')
     sud = request.get_json()
-    if 'name' not in sud.keys():
-      return (abort(400, 'Missing name'))
     for key, value in sud.items():
-      if key not in ['id', 'created_at', 'updated_at']:
+      if key not in ['id', 'email', 'created_at', 'updated_at']:
         setattr(state_obj, key, value)
     state_obj.save()
-    return ((jsonify(state_obj.to_dict()), 200))
+    return (jsonify(state_obj.to_dict()), 200)
   if request.method == 'DELETE':
     storage.delete(state_obj)
     del state_obj
