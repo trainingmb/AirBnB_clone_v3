cc5ff4b5d0d52a413507c787eb809fa9aae592dd
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-07 14:57:27 +0300

    Update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,49 +4,60 @@ Index for V1
 """
 from api.v1.views import app_views, jsonify, abort, request
 from models import storage
-from models.state import State
+from models.cities import City
+from model.users import User
+from model.places import Place
 
 
-@app_views.route('/states', methods = ['POST', 'GET'])
-def all_states():
+@app_views.route('/cities/<city_id>/places', methods=['POST', 'GET'], strict_slashes=False)
+def all_places(city_id):
   """
   Returns a list of all states
   """
-  if request.method == 'GET':
-    states = [i.to_dict() for i in storage.all(State).values()]
-    return (jsonify(states))
-  if request.method == 'POST':
-    if not request.is_json:
-      return abort(400, 'Not a JSON')
-    sud = request.get_json()
-    newstate_obj = State(**sud)
-    newstate_obj.save()
-    return ((jsonify(newstate_obj.to_dict()), 200))
-
-@app_views.route('/states/<state_id>', methods = ['POST', 'GET', 'DELETE'])
-def rud_state(state_id):
-  """
-  Get/Modify/Delete state with id <state_id>
-  if present else returns raises error 404
-  """
-  state_obj = storage.get(State,state_id)
-  if state_obj is None:
+  city_obj = storage.get(City,city_id)
+  if city_obj is None:
     abort(404)
   if request.method == 'GET':
-    return (jsonify(state_obj.to_dict()))
+    places = [i.to_dict() for i in city_obj.places]
+    return (jsonify(places))
   if request.method == 'POST':
     if not request.is_json:
       return abort(400, 'Not a JSON')
     sud = request.get_json()
+    if 'user_id' not in sud.keys():
+      abort(400, 'Missing user_id')
+    else:
+      user_obj = storage.get(User,user_id)
+      if user_obj is None:
+        abort(404)
     if 'name' not in sud.keys():
-      return (abort(400, 'Missing name'))
+      abort(400, 'Missing name')
+    newplace_obj = Place(**sud)
+    newplace_obj.save()
+    return (jsonify(newplace_obj.to_dict()), 201)
+
+@app_views.route('/places/<place_id>', methods=['PUT', 'GET', 'DELETE'], strict_slashes=False)
+def rud_place(state_id):
+  """
+  Get/Modify/Delete place with id <place_id>
+  if present else returns raises error 404
+  """
+  place_obj = storage.get(Place,place_id)
+  if place_obj is None:
+    abort(404)
+  if request.method == 'GET':
+    return (jsonify(place_obj.to_dict()))
+  if request.method == 'PUT':
+    if not request.is_json:
+      abort(400, 'Not a JSON')
+    sud = request.get_json()
     for key, value in sud.items():
-      if key not in ['id', 'created_at', 'updated_at']:
-        setattr(state_obj, key, value)
-    state_obj.save()
-    return ((jsonify(state_obj.to_dict()), 200))
+      if key not in ['id', 'user_id', 'created_at', 'updated_at']:
+        setattr(place_obj, key, value)
+    place_obj.save()
+    return (jsonify(place_obj.to_dict()), 200)
   if request.method == 'DELETE':
-    storage.delete(state_obj)
-    del state_obj
+    storage.delete(place_obj)
+    del place_obj
     storage.save()
     return (jsonify({}), 200)
