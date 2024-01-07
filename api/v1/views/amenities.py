9e0dae93e3c4495b8aa0d0812b9f03b24bdef4ce
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-07 13:59:39 +0300

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,52 +1,54 @@
 #!/usr/bin/python3
 """
-Index for V1
+API Base for amenity based actions
 """
 from api.v1.views import app_views, jsonify, abort, request
 from models import storage
-from models.state import State
+from models.amenity import Amenity
 
 
-@app_views.route('/states', methods = ['POST', 'GET'])
-def all_states():
+@app_views.route('/amenities', methods = ['POST', 'GET'])
+def all_amenities():
   """
-  Returns a list of all states
+  Returns a list of all amenities
   """
   if request.method == 'GET':
-    states = [i.to_dict() for i in storage.all(State).values()]
-    return (jsonify(states))
+    amenities = [i.to_dict() for i in storage.all(Amenity).values()]
+    return (jsonify(amenities))
   if request.method == 'POST':
     if not request.is_json:
-      return abort(400, 'Not a JSON')
+      abort(400, 'Not a JSON')
     sud = request.get_json()
-    newstate_obj = State(**sud)
-    newstate_obj.save()
-    return ((jsonify(newstate_obj.to_dict()), 200))
+    if 'name' is not in sud.keys():
+      abort(400, 'Missing name')
+    newamenity = Amenity(**sud)
+    newamenity.save()
+    return ((jsonify(newamenity.to_dict()), 201))
 
-@app_views.route('/states/<state_id>', methods = ['POST', 'GET', 'DELETE'])
-def rud_state(state_id):
+@app_views.route('/amenities/<amenity_id>', methods = ['PUT', 'GET', 'DELETE'])
+def rud_amenity(amenity_id):
   """
-  Get/Modify/Delete state with id <state_id>
+  Get/Modify/Delete amenity with id <amenity_id>
   if present else returns raises error 404
   """
-  state_obj = storage.get(State,state_id)
-  if state_obj is None:
+  amenity_obj = storage.get(Amenity, amenity_id)
+  if amenity_obj is None:
     abort(404)
   if request.method == 'GET':
-    return (jsonify(state_obj.to_dict()))
-  if request.method == 'POST':
+    return (jsonify(amenity_obj.to_dict()))
+  if request.method == 'PUT':
     if not request.is_json:
-      return abort(400, 'Not a JSON')
+      abort(400, 'Not a JSON')
     sud = request.get_json()
     if 'name' not in sud.keys():
-      return (abort(400, 'Missing name'))
+      abort(400, 'Missing name')
     for key, value in sud.items():
       if key not in ['id', 'created_at', 'updated_at']:
-        setattr(state_obj, key, value)
-    state_obj.save()
-    return ((jsonify(state_obj.to_dict()), 200))
+        setattr(amenity_obj, key, value)
+    amenity_obj.save()
+    return ((jsonify(amenity_obj.to_dict()), 200))
   if request.method == 'DELETE':
-    storage.delete(state_obj)
-    del state_obj
+    storage.delete(amenity_obj)
+    del amenity_obj
     storage.save()
     return (jsonify({}), 200)
