e498246b50c5fb59d7acd34f1b311ca862a86e05
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-09 02:43:46 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,18 +10,20 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-  """
-  Closes the storage on teardown
-  """
-  storage.close()
+    """
+    Closes the storage on teardown
+    """
+    storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-  """
-  Handler for page not found
-  """
-  return (jsonify({'error': 'Not Found'}), 404)
+    """
+    Handler for page not found
+    """
+    return (jsonify({'error': 'Not Found'}), 404)
+
 
 if __name__ == '__main__':
-  from api.v1 import HBNB_API_HOST, HBNB_API_PORT
-  app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    from api.v1 import HBNB_API_HOST, HBNB_API_PORT
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
