630ec181a9e50eb09f1efd159bfa03900d2e9c23
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-09 02:33:28 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,10 @@
 Index for V1
 """
 from api.v1.views import app_views, jsonify
+from models import storage
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
-from models import storage
 from models.place import Place
 from models.review import Review
 from models.state import State
