cc25162659453b67f9936e7288a6a7999c7baba4
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-09 03:33:08 +0300

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -15,7 +15,7 @@ def city_states(state_id):
     """
     Relates city and states
     """
-    state_obj = storage.get(State,state_id)
+    state_obj = storage.get(State, state_id)
     if state_obj is None:
         abort(404)
     if request.method == 'GET':
@@ -32,28 +32,29 @@ def city_states(state_id):
         newcity.save()
         return (jsonify(newcity.to_dict()), 201)
 
+
 @app_views.route('/cities/<city_id>', methods=['PUT', 'GET', 'DELETE'])
 def rud_city(city_id):
-  """
-  Get/Modify/Delete city with id <city_id>
-  if present else returns raises error 404
-  """
-  city_obj = storage.get(City, city_id)
-  if city_obj is None:
-      abort(404)
-  if request.method == 'GET':
-      return (jsonify(city_obj.to_dict()))
-  if request.method == 'PUT':
-      if not request.is_json:
-          abort(400, 'Not a JSON')
-      sud = request.get_json()
-      for key, value in sud.items():
-          if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(city_obj, key, value)
-      city_obj.save()
-      return (jsonify(city_obj.to_dict()), 200)
-  if request.method == 'DELETE':
-      storage.delete(city_obj)
-      del city_obj
-      storage.save()
-      return (jsonify({}), 200)
+    """
+    Get/Modify/Delete city with id <city_id>
+    if present else returns raises error 404
+    """
+    city_obj = storage.get(City, city_id)
+    if city_obj is None:
+        abort(404)
+    if request.method == 'GET':
+        return (jsonify(city_obj.to_dict()))
+    if request.method == 'PUT':
+        if not request.is_json:
+            abort(400, 'Not a JSON')
+        sud = request.get_json()
+        for key, value in sud.items():
+            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+                setattr(city_obj, key, value)
+        city_obj.save()
+        return (jsonify(city_obj.to_dict()), 200)
+    if request.method == 'DELETE':
+        storage.delete(city_obj)
+        del city_obj
+        storage.save()
+        return (jsonify({}), 200)
