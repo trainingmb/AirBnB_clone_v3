28e9619f4e98c99653d5c67a92b2b1b216001972
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-09 00:18:58 +0000

    Cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,13 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
