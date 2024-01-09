5c23667f83b43c3d181cd3a184619c058aa7771b
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-09 03:26:20 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,11 @@
 Init for Views in V1
 """
 from flask import Blueprint, jsonify, abort, request
+
+
+app_views = Blueprint('ap_views', __name__, url_prefix="/api/v1")
+
+
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
@@ -10,5 +15,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
-
-app_views = Blueprint('ap_views', __name__, url_prefix="/api/v1")
