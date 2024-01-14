0709b52989e9f6c6b543c88458e1afd83d93db58
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-15 00:55:02 +0300

    Update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -82,8 +82,8 @@ def search_places():
         sud = request.get_json()
         if len(sud) == 0 or \
            (sum([len(sud[i]) for i in sud.keys()]) == 0):
-                 return (jsonify([i.to_dict()
-                                  for i in storage.all(Place).values()]))
+               return (jsonify([i.to_dict()
+                                for i in storage.all(Place).values()]))
         cities = []
         for st_id in sud.get('states', []):
             st_obj = storage.get(State, st_id)
